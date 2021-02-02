6c3c35d3eaf948c97597230fe62217f52e8a6167
Rizlkmil <rizalkmil2007@gmail.com>
2021-02-02 13:54:02 +0700

    ini bot om

---
+++ src/daftarvip.js
@@ -0,0 +1,12 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 5K > Akses Fitur ViP
+-Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/6282137918354 atau ketik *${prefix}owner* `
+}
+exports.daftarvip = daftarvip
